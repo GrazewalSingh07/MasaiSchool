1cf54358943bbae29e40c483da9418c058902a25
GrazewalSingh <singhgrazewal2@gmail.com>
2022-03-28 10:58:21 +0530

    C4eval

---
+++ C4Eval/src/index.js
@@ -0,0 +1,11 @@
+const express= require("express");
+const app= express()
+app.use(express.json())
+module.exports= app
+ 
+const register= require("./controlller/register.controller")
+ const login= require("./controlller/login.controller")
+ const todocontroller= require("./controlller/todo.Controller")
+app.use("/register", register);
+app.use("/login", login)
+app.use("/todo", todocontroller)
